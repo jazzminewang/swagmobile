b1e7216d1bba704f0dcf698a6ebcf7ee7b6753c5
Jasmine Wang <jasminewang76@gmail.com>
2017-09-23 17:06:25 -0400

    playing around

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, Image, View } from 'react-native'
+import RoundedButton from '../Components/RoundedButton.js'
 import DevscreensButton from '../../ignite/DevScreens/DevscreensButton.js'
 
 import { Images } from '../Themes'
@@ -14,16 +15,18 @@ export default class LaunchScreen extends Component {
         <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
         <ScrollView style={styles.container}>
           <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
+            {/* <Image source={Images.launch} style={styles.logo} /> */}
           </View>
 
           <View style={styles.section} >
             <Image source={Images.ready} />
             <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
+              Trust us, swag overflow is actually a good thing.  
             </Text>
           </View>
 
+          <RoundedButton text="Login" /> 
+          <RoundedButton text="Search" />
           <DevscreensButton />
         </ScrollView>
       </View>
